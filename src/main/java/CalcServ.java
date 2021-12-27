138ac03e91bfb3ca23b7eb4c826ec6e3a5d49153
suhail <suhailkhan5106@gmail.com>
2021-12-27 12:11:35 +0530

    testing

---
+++ src/main/java/CalcServ.java
@@ -0,0 +1,41 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+public class CalcServ extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		PrintWriter out = response.getWriter();
+		double x;
+		double y;
+		String str="";
+		x=Double.parseDouble(request.getParameter("num1"));
+		y=Double.parseDouble(request.getParameter("num2"));
+		str=request.getParameter("operation");
+		if(str.equals("add"))
+			out.println("<h1>Result of Addition is: "+ (x+y)+ "</h1>");
+		else if(str.equals("sub"))
+			out.println("<h1>Result of Subtraction is: "+ (x-y)+ "</h1>");
+		else if(str.equals("mul"))
+			out.println("<h1>Result of Multiplication is: "+ (x*y)+ "</h1>");
+		else if(str.equals("div"))
+			if(y==0) {
+				out.println("<h1>It cannot bt zero it deals infinity as answer");
+			}
+			else out.println("<h1>Result of Division is: "+ (x/y)+ "</h1>");
+		
+	}
+
+	
+}
---
+++ src/main/webapp/index.html
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<form action="xy" method="get">
+	<h1> Calculator</h1>
+	   Enter First no.: <input type="text" name="num1"/><br>
+	   Enter second no.: <input type="text" name="num2"/><br>
+	   Select the operation:<br/>
+	   <input type="radio" name="operation" value="add">Addition
+	   <input type="radio" name="operation" value="sub">Subtraction
+	   <input type="radio" name="operation" value="mul">Multiplication
+	   <input type="radio" name="operation" value="div">Division<br>
+	   <input type="submit" value="Calculate"/>
+	   <input type="reset" value="Reset"/>
+	</form>
+</body>
+</html>
